90907464b34c376ddd98e74163cba0e7f4dfaa6f
Felipe <copyandpaste0@gmail.com>
2025-01-12 23:39:17 -0600

    Simplify aggregator pipeline logic and find added/removed games quickly

--- app/helpers/NewGamesChecker.py
+++ app/helpers/NewGamesChecker.py
@@ -14,6 +14,17 @@ from app.helpers.enum.SteamEnums import SteamEnums
 ###
 
 class NewGamesChecker:
+    class GameData(object):
+        def __init__(self, appid, name):
+            self.appid = appid
+            self.name = name
+        def __hash__(self):
+            return hash((self.appid, self.name))
+        def __eq__(self, other):
+            if not isinstance(other, type(self)): return NotImplemented
+            return self.appid == self.appid
+        def __str__(self):
+            return f'appId: {self.appid}, name: {self.name}'
 
     logging.config.fileConfig('app/log/logging.conf')
     loggerConsole = logging.getLogger('ConsoleLogger')
@@ -21,7 +32,7 @@ class NewGamesChecker:
     def is_new_game():
         NewGamesChecker.loggerConsole.info("Beginning check for a new game")
         #NewGamesChecker.download_all_games()
-        NewGamesChecker.check_for_new_game()
+        NewGamesChecker.check_for_changes()
 
     def download_all_games():
         # Helper method to demonstrate from /data/local file first, before querying the website.
@@ -41,18 +52,85 @@ class NewGamesChecker:
                 f.write(all_games.text) #all_games.text
             f.close()
 
-    def check_for_new_game():
+    def check_for_changes():
+
+        yesterday = "data/all_games_2025-01-09.json.tmp"
+        today = "data/all_games_2025-01-10.json.tmp"
+        # yesterday = "data/all_games_1.tmp"
+        # today = "data/all_games_2.tmp"
 
         NewGamesChecker.loggerConsole.info("Checking for change")
         # Shallow set to true use os.stat() signatures (file type, size, modification) and not byte-by-byte
-        result = filecmp.cmp("data/all_games_2025-01-09.json.tmp", "data/all_games_2025-01-10_old.json.tmp", shallow=False)
+        result = filecmp.cmp(yesterday, today, shallow=False)
 
         if result:
-            print("The files are identical.")
+            NewGamesChecker.loggerConsole.info("The files are identical.")
         else:
-            print("The files are different.")
+            NewGamesChecker.loggerConsole.info("The files are different. Finding differences...")
 
+            f = open("data/added_games_set.log", "a")
 
+            removed_games = NewGamesChecker.find_diff_games(yesterday, today)
+            added_games = NewGamesChecker.find_diff_games(today, yesterday)
+
+            f.write("Count: " + str(len(removed_games)) + "\n")
+            for game in added_games: f.write(str(game) + "\n")
+            f.write("Count: " + str(len(added_games)) + "\n")
+            for game in removed_games: f.write(str(game) + "\n")
+
+            f.close()
+
+    def find_diff_games(dateX, dateY):
+
+        NewGamesChecker.loggerConsole.info("Searching for different games")
+
+        first_games_set = set()
+        second_games_set = set()
+
+        with open(dateX, 'r') as json_file:
+            yesterday_dict = json.load(json_file)
+            for game in yesterday_dict['applist']['apps']:
+                first_games_set.add(NewGamesChecker.GameData(game['appid'], game['name']))
+        json_file.close()
+
+        with open(dateY, 'r') as json_file:
+            today_dict = json.load(json_file)
+            for game in today_dict['applist']['apps']:
+                second_games_set.add(NewGamesChecker.GameData(game['appid'], game['name']))
+        json_file.close()
+
+        different_games = first_games_set.difference(second_games_set)
+
+        return different_games
+        """
+        yesterdays_games = yesterday_dict['applist']['apps']
+        todays_games = today_dict['applist']['apps']
+
+        counter = 0
+
+        diff_games = []
+
+        # Checks for a different game, be it removed or added
+        for y_keyval in yesterdays_games:
+            print("Checking for " + str(y_keyval['appid']))
+            counter += 1
+            percentage = counter / 230000 * 100
+            print(percentage)
+            for t_keyval in todays_games:
+                game_found = False
+                if y_keyval['appid'] == t_keyval['appid']:
+                    game_found = True
+                    # print("Appids match, breaking: " + str(y_keyval['appid']) + ", " + str(y_keyval['appid']))
+                    break
+                else:
+                    pass
+                    # print("No match, continuing search: " + str(y_keyval['appid']) + ", " + str(t_keyval['appid']) )
+            if not game_found:
+                # print("Appid does not exist, storing unique value: " + str(y_keyval))
+                diff_games.append(y_keyval)
+
+        return diff_games
+        """
 
 # TODO with new game checker
 # 1) use a hash to see if there is a new game (done)
